08a2d3ebc48df4a2be31901e48e9a5a7ca9fc96c
omirbeck <ommirtull@gmail.com>
2020-06-18 03:08:47 +0600

    feat: get new words, quantity cards

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,10 @@ const QUANTITY_WORDS = document.querySelector('.quantity-words');
 const QUANTITY_CARDS = document.querySelector('.quantity-cards');
 const MAX_PAGE = 29;
 const MAX_GROUP = 5;
+let allCards = []; 
+const arrayNewWords = [];
+const arrayOldWords = [];
+const arrayId = ["5e9f5ee35eb9e72bc21af4a1","5e9f5ee35eb9e72bc21af4a2","5e9f5ee35eb9e72bc21af4a3","5e9f5ee35eb9e72bc21af4a4"];
 
 
 const getQuantityWords = async (countPage, countGroup) => {
@@ -53,26 +57,69 @@ const getQuantityWords = async (countPage, countGroup) => {
 
 
 const getNewWords = async () => {
-    const arrayId = ["5e9f5ee35eb9e72bc21af4a1","5e9f5ee35eb9e72bc21af4a2","5e9f5ee35eb9e72bc21af4a3","5e9f5ee35eb9e72bc21af4a4"];
-    const arrayNewWords = [];
+    arrayNewWords.length = 0;
+    arrayOldWords.length = 0;
     const quantityNewWords = QUANTITY_WORDS.value;
     let countPage =  Math.ceil(Number(quantityNewWords)/20);
     let countGroup = Math.ceil(Number(countPage/600));
 
-    const quantityWords = await getQuantityWords(countPage, countGroup);
+    const findNewWords = async () => {
+        const quantityWords = await getQuantityWords(countPage, countGroup);
 
-    quantityWords.forEach((word, item) => {
-        if (quantityNewWords < item) {
-            arrayNewWords.push(word)
+        quantityWords.forEach((word) => {
+            if (arrayId.indexOf(word.id) === -1) {
+                arrayNewWords.push(word);
+            } else {
+                arrayOldWords.push(word)
+            }
+        });
+
+        if (arrayNewWords.length < quantityNewWords) {
+            if (countPage < MAX_PAGE) {
+                countPage += 1;
+            } else {
+                countPage = 0;
+                countGroup += 1;
+            }
+            console.log('if <')
+            findNewWords();
+        } else {
+            console.log('else')
+            while (arrayNewWords.length > quantityNewWords) {
+                console.log('while')
+                arrayNewWords.pop();
+            };
         }
-    });
+        console.log(arrayNewWords,arrayOldWords);
+
+    }
+
+    return findNewWords();
+    
 }
+
+const quantityCards = () => {
+
+    const countCards = QUANTITY_CARDS.value;
+    const quantityNewWords = QUANTITY_WORDS.value;
+
+    if (quantityNewWords > countCards) {
+        console.log(quantityNewWords, countCards)
+        const arrayCards = arrayNewWords.concat(arrayOldWords);
+        allCards = arrayCards.filter((word, item) => item < countCards);
+    } else {
+        console.log(`Минимально можно выбрать ${quantityNewWords} карточек`);
+    }
+
+}
+
 
 const renderCard = async () => {
 
-    const data = await getWords(page, group);
+    //const data = await getWords(page, group);
+    console.log(allCards)
 
-    let {textExample,textExampleTranslate,textMeaning,textMeaningTranslate,transcription,word,wordTranslate,audio} = data[count];
+    let {textExample,textExampleTranslate,textMeaning,textMeaningTranslate,transcription,word,wordTranslate,audio} = allCards[count];
     curentWord = word;
     console.log(textExample,textExampleTranslate,textMeaning,textMeaningTranslate,transcription,word,wordTranslate);
     const card = `<span class="text-example">${textExample}</span>
@@ -111,12 +158,14 @@ const audioPlay = (src) => {
         audio.play();
 }
 
-renderCard();
+
 
 const BTN_CHECK = document.querySelector('.btn-check');
 
-BTN_CHECK.addEventListener('click', () => {
+BTN_CHECK.addEventListener('click', async () => {
     receivedWords.length = 0;
-    getQuantityWords();
-    checkWord();
+    await getNewWords();
+    quantityCards();
+    renderCard();
+    //checkWord();
 });
