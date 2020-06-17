ee1a31e98b87fd0668832b17449a4d83ce337164
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-06-17 18:25:27 +0300

    feat: add sample start page

--- src/js/speak-it/speak-it.js
+++ src/js/speak-it/speak-it.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mediaData = 'https://raw.githubusercontent.com/anastasiyagolunova/rslang-data/master/files/';
+const getWords = async (page, group) => {
+    const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
+    const res = await fetch(url);
+    const json = await res.json();
+    console.log(JSON.stringify(json, null, 1));
+
+};
+
+getWords(0, 0);
