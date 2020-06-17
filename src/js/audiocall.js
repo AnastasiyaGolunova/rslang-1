d20052b208ef61859a92c155000a582e2d6531a5
Natalya POida <nataly.poida@gmail.com>
2020-06-17 16:10:57 +0600

    feat:add start game button event and audio

--- src/js/audiocall.js
+++ src/js/audiocall.js
@@ -1,11 +1,31 @@
 const urlApi = 'https://afternoon-falls-25894.herokuapp.com/words';
+const dataUrl = 'https://raw.githubusercontent.com/natalypoida/rslang-data/master/';
 const main = document.querySelector('#main');
-let audio = {
-    correct: new Audio(''),
-    errors: new Audio(''),
-    success: new Audio(''),
-    failure: new Audio(''),
-};
+const startButton = document.querySelector('#start-button');
+const answerButtons = document.querySelector('#answer-buttons');
+const dontKnowButton = document.querySelector('#dont-know-button');
+const audioCall = document.querySelector('#audio');
+const leoImage = document.querySelector('#leo');
+const description = document.querySelector('#description');
+const correctWord = document.querySelector('#correct');
+//let audio = {
+  //  correct: new Audio(''),
+    //errors: new Audio(''),
+    //success: new Audio(''),
+    //failure: new Audio(''),
+//};
+
+function startGame() {
+    startButton.classList.add('hide');
+    leoImage.classList.add('hide');
+    description.classList.add('hide');
+    answerButtons.classList.remove('hide');
+    audioCall.classList.remove('hide');
+    dontKnowButton.classList.remove('hide');
+}
+
+startButton.addEventListener('click', startGame);
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
@@ -18,7 +38,19 @@ function getData(){
     })
     .then((data) => {
         console.log(data);
-    }); 
+        let i = getRandomInt(20);
+        console.log(i);
+        let soundSrc = data[i].audio;
+        let imageSrc = data[i].image;
+        let pageSoundUrl = `${dataUrl}${soundSrc}`;
+        let pageImageUrl = `${dataUrl}${imageSrc}`;
+        console.log(pageImageUrl);
+        console.log(pageSoundUrl);
+        audioCall.setAttribute('src', `${pageSoundUrl}`);
+        audio.play();
+        
+    })
+    ; 
 }
 
 getData();
