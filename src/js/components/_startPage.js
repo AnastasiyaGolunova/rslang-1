7fc628fb3a58ae61cb68c4da7b78896de5e14864
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-06-17 16:28:48 +0600

    feat: add timer logic

--- src/js/components/_startPage.js
+++ src/js/components/_startPage.js
@@ -1,6 +1,7 @@
 //start page creation
 
 const startPage = document.createElement("div");
+
 startPage.classList.add("page-wrapper");
 
 startPage.innerHTML = `<div class="start-page">
@@ -14,3 +15,41 @@ you'll get.</p></div>
 </div>
 </div>`;
 document.body.prepend(startPage);
+
+//Start button click
+const startBtn = document.querySelector(".start-btn");
+startBtn.addEventListener("click", () => {
+  document.querySelector(".body-block").classList.toggle("hidden");
+
+  //timer
+  function timer(from, to) {
+    let current = from;
+
+    startPage.innerHTML = `
+    <div class="game-page">
+
+    <div class="timer">
+    <div class="circle-timer">
+        <div class="timer-slot">
+            <div class="timer-lt"></div>
+        </div>
+        <div class="timer-slot">
+            <div class="timer-rt"></div>
+        </div>
+        <div class="count"></div>
+    </div>
+  </div>
+  </div>`;
+
+    const timerId = setInterval(function () {
+      document.querySelector(".count").innerHTML = current;
+      if (current == to) {
+        clearInterval(timerId);
+        document.querySelector(".circle-timer").classList.toggle("hidden");
+        //loadGame();
+      }
+      current--;
+    }, 1000);
+  }
+  timer(3, 0);
+});
