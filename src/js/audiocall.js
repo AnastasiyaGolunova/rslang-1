160adba40c92600ccaf07fdf7e09bbeeb72b70af
Natalya POida <nataly.poida@gmail.com>
2020-06-19 19:52:34 +0600

    feat: add words from data to answer buttons

--- src/js/audiocall.js
+++ src/js/audiocall.js
@@ -10,6 +10,8 @@ const description = document.querySelector('#description');
 const correctWord = document.querySelector('#correct');
 const select = document.querySelector('#select');
 const soundImage = document.querySelector('#sound');
+
+let shuffleAnswers, currentAnswerIndex;
 //let audio = {
   //  correct: new Audio(''),
     //errors: new Audio(''),
@@ -26,6 +28,7 @@ function startGame() {
     dontKnowButton.classList.remove('hide');
     soundImage.classList.remove('hide');
     getData();
+    
 }
 
 startButton.addEventListener('click', startGame);
@@ -35,7 +38,7 @@ function getRandomInt(max) {
   }
 function getData(){
     let group = select.options[select.selectedIndex].value;
-    let url = `${urlApi}?page=${getRandomInt(31)}&group=${group}`;
+    let url = `${urlApi}?page=${getRandomInt(30)}&group=${group}`;
     fetch(url)
     .then((response) => {
         let data = response.json();
@@ -47,14 +50,48 @@ function getData(){
         console.log(i);
         let soundSrc = data[i].audio;
         let imageSrc = data[i].image;
+        let correctAnswer = data[i].word;
         let pageSoundUrl = `${dataUrl}${soundSrc}`;
         let pageImageUrl = `${dataUrl}${imageSrc}`;
+        data.splice(i, 1);
+        let newData = data;
+        console.log(newData);
+        let j = getRandomInt(14);
+        let firstWord = newData[j].word;
+        let secondWord = newData[j +=1].word;
+        let thirdWord = newData[j +=2].word;
+        let forthWord = newData[j +=3].word;
+        let answers = [
+            {text: `${correctAnswer}`, correct: true},
+            {text: `${firstWord}`, correct: false},
+            {text: `${secondWord}`, correct: false},
+            {text: `${thirdWord}`, correct: false},
+            {text: `${forthWord}`, correct: false},
+        ];
+        currentAnswerIndex = 0;
+    answers.forEach(element => {
+    const button = document.createElement('button');
+    button.innerText = element.text;  
+    button.classList.add('button')
+    if (element.correct) {
+        button.dataset.correct = element.correct;
+    }
+    //button.addEventListener('click', selectAnswer);
+    answerButtons.appendChild(button);
+    
+    });
+        console.log(answers);
+        console.log(firstWord, secondWord, thirdWord, forthWord);
+        console.log(j);
         console.log(pageImageUrl);
         console.log(pageSoundUrl);
+        console.log(correctAnswer);
         audioCall.setAttribute('src', `${pageSoundUrl}`);
         audio.play();
         
+        
     })
+    
     ; 
 };
 
