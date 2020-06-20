0b1e55c8f655aab70d6193ab4ef8444b9c4a7e5e
Natalya POida <nataly.poida@gmail.com>
2020-06-20 17:32:29 +0600

    feat: add i-dont-know-button event, color changing to display correct answer

--- src/js/audiocall.js
+++ src/js/audiocall.js
@@ -10,7 +10,9 @@ const description = document.querySelector('#description');
 const correctWord = document.querySelector('#correct');
 const select = document.querySelector('#select');
 const soundImage = document.querySelector('#sound');
-
+const correctWordImage = document.querySelector('#correct-image');
+const nextButton = document.querySelector('#next-button');
+const answerText = document.querySelector('#correct-answer');
 let shuffleAnswers, currentAnswerIndex;
 //let audio = {
   //  correct: new Audio(''),
@@ -36,6 +38,40 @@ startButton.addEventListener('click', startGame);
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+  function clearStatusClass(element) {
+      element.classList.remove('correct');
+      element.classList.remove('wrong');
+  }
+ function setStatusClass(element, correct) {
+     clearStatusClass(element);
+     if(correct) {
+         element.classList.add('correct')
+     } else {
+         element.classList.add('wrong');
+     }
+ }
+  function selectAnswer(element) {
+      const selectedButton = element.target;
+      const correct = selectedButton.dataset.correct;
+      setStatusClass(document.body, correct);
+      Array.from(answerButtons.children).forEach(button => {
+          setStatusClass(button, button.dataset.correct)
+      })
+      
+  }
+  function setCorrectAnswer() {
+    correctWordImage.classList.remove('hide');
+    nextButton.classList.remove('hide');
+    soundImage.classList.add('hide');
+    dontKnowButton.classList.add('hide');
+    answerText.classList.remove('hide');
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+
+}
+  
 function getData(){
     let group = select.options[select.selectedIndex].value;
     let url = `${urlApi}?page=${getRandomInt(30)}&group=${group}`;
@@ -51,16 +87,20 @@ function getData(){
         let soundSrc = data[i].audio;
         let imageSrc = data[i].image;
         let correctAnswer = data[i].word;
+        let answerTranscription = data[i].transcription;
+        let answerTranslation = data[i].wordTranslate;
         let pageSoundUrl = `${dataUrl}${soundSrc}`;
         let pageImageUrl = `${dataUrl}${imageSrc}`;
+        console.log(pageImageUrl);
         data.splice(i, 1);
         let newData = data;
         console.log(newData);
-        let j = getRandomInt(14);
+        let j = getRandomInt(16);
+        console.log(j);
         let firstWord = newData[j].word;
-        let secondWord = newData[j +=1].word;
-        let thirdWord = newData[j +=2].word;
-        let forthWord = newData[j +=3].word;
+        let secondWord = newData[j + 1].word;
+        let thirdWord = newData[j + 2].word;
+        let forthWord = newData[j + 3].word;
         let answers = [
             {text: `${correctAnswer}`, correct: true},
             {text: `${firstWord}`, correct: false},
@@ -76,24 +116,28 @@ function getData(){
     if (element.correct) {
         button.dataset.correct = element.correct;
     }
-    //button.addEventListener('click', selectAnswer);
+    button.addEventListener('click', selectAnswer) 
+         
+    
     answerButtons.appendChild(button);
     
     });
         console.log(answers);
         console.log(firstWord, secondWord, thirdWord, forthWord);
-        console.log(j);
         console.log(pageImageUrl);
         console.log(pageSoundUrl);
         console.log(correctAnswer);
         audioCall.setAttribute('src', `${pageSoundUrl}`);
         audio.play();
-        
+        correctWordImage.setAttribute('src', `${pageImageUrl}`);
+        answerText.innerText=`${correctAnswer} - ${answerTranscription} - ${answerTranslation}`;
         
     })
     
     ; 
 };
+
+dontKnowButton.addEventListener('click', setCorrectAnswer)
 
 
 
