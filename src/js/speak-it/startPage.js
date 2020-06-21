c3572adcc93e8dfbd2111224c5dbec8075396483
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-06-21 17:33:12 +0300

    feat: add sample append data

--- src/js/speak-it/startPage.js
+++ src/js/speak-it/startPage.js
@@ -4,4 +4,6 @@ const btn = document.querySelector('.start');
 
 btn.addEventListener('click', function () {
     document.querySelector('.wrap').classList.add('block');
+    document.querySelector('.wrap_game').classList.remove('wrap_game');
 })
+
