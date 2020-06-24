0aac88fb5bcb566fec2f393c9bd7175efe96573f
SergeyShapkarin <sergey.shapkarin@mail.ru>
2020-06-25 00:45:07 +0600

    add: createCanvasElemens

--- src/js/english-puzzle.js
+++ src/js/english-puzzle.js
@@ -7,6 +7,7 @@ import {
     pageGame,
 } from './constants.js';
 import images from './images.js';
+import createCanvasElements from './createCanvasElements.js';
 
 
 function showPuzzle() {
@@ -33,6 +34,22 @@ startButton.onclick = () => {
     document.querySelector('.img-puzzle').style.backgroundImage = `url('${images[image]}')`;
 
     const listSentense = getSentense();
+    listSentense.then(data => {
+        console.log(data);
+        console.log(data.map(item => item.textExample),)
+        createCanvasElements({
+            src: `${images[image]}`,
+            wordsList: data.map(item => item.textExample),
+          }).then(res => {
+            const imageContainer = document.querySelector('.img-puzzle');
+            imageContainer.style.backgroundColor = '#020c97';
+            imageContainer.style.border = '2px solid black';
+            imageContainer.style.backgroundImage = 'none';
+            imageContainer.append(...res);
+            // document.querySelector(".img-puzzle");
+          })
+    })
+
     
 
     showPuzzle();
