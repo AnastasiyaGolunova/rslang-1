c75825983da31ad22eab0bfe21e9a8c235bbd755
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-06-25 18:48:31 +0300

    fix: translate

--- src/js/speak-it/learn.js
+++ src/js/speak-it/learn.js
@@ -5,20 +5,15 @@ cardsWrap.addEventListener('click', function learn (event) {
     if(check.matches('.fa')){
         const atr = event.target.getAttribute('data-active');
         console.log(atr);
-        const {audio, image, transcription, word} = arr[atr];
+        const {audio, image, wordTranslate} = arr[atr];
         const sound = document.createElement('audio');
         sound.src = `${mediaData}${audio}`;
         sound.play();
         const pic = `${mediaData}${image}`;
         imgTrain.src = pic;
-        // const wTranscript = transcription;
-        // wordTranscript.innerHTML = wTranscript;
-        async function tr () {
-            const wTranslate = await getTranslation(word);
-            wordTranslate.textContent = wTranslate;
-            console.log(wTranslate);
-        }
-        tr();
+        const wTranslate = wordTranslate;
+        wdTranslate.textContent = wTranslate;
+        console.log(wordTranslate);
     }
     strGame.addEventListener('click', function (event) {
         const start = event.target;
