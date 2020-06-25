e45fe3212a2bdd65b35a4021606f47a676ab814d
Zarina <zhiyenbayeva.z@gmail.com>
2020-06-25 15:05:02 +0600

    feat: fix styles for mobile view

--- src/js/login.js
+++ src/js/login.js
@@ -2,10 +2,12 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
+export default function login() {
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
 
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-});
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+}
