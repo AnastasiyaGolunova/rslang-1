e8ca066d3380f4a19b234ab0f9792d2e6d2c8fbd
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-06-28 19:35:22 +0300

    feat: add sound on right answer + append rec word and fix fight/wrong answers

--- src/js/speak-it/game.js
+++ src/js/speak-it/game.js
@@ -7,27 +7,40 @@ const strGame = document.querySelector('.speak');
 
 let recognizer = new window.webkitSpeechRecognition || window.SpeechRecognition;
 recognizer.continuous = true;
-//recognizer.interimResults = true;
+recognizer.interimResults = true;
 recognizer.lang = 'en-En';
+
 recognizer.onresult = function (event) {
     let result = event.results[event.resultIndex];
-
     let elem = result[0].transcript;
     console.log(elem);
-    console.log(words);
-    if (result.isFinal) {
-        if(words.includes(elem)){
-            console.log('right');
-        } else {console.log('wrong');}
-        console.log('Вы сказали: ' + result[0].transcript);
-    } else {
-        console.log('Промежуточный результат: ', result[0].transcript);
-    }
-};
 
+    if (result.isFinal) {
+        function contains(words, elem) {
+            const res = words.indexOf(elem.trim()) !== -1;
+            const wTranscript = elem;
+            wdTranslate.textContent = wTranscript;
+            return res;
+        }
+        if (contains(words, elem)) {
+            const anw = new Audio('./audio/right.wav');
+            anw.play();
+            // const star = document.createElement('i');
+            // star.classList = 'fas fa-star';
+            //document.querySelector('.card').appendChild(star);
+            console.log('right');
+        } else {
+            console.log('wrong');
+        }
+        console.log('Вы сказали: ' + result[0].transcript);
+        // } else {
+        //     console.log('Промежуточный результат: ', result[0].transcript);
+        // }
+    };
+}
 strGame.addEventListener('click', function () {
     cardsWrap.removeEventListener('click', learn);
     recognizer.start();
+    // wdTranslate.add.style = 'line';
     console.log('game');
 });
-console.log(words);
