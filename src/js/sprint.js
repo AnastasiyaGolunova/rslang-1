c2a55e79120ff0e2fa1af664659494957e3020c4
yauheniche <cherkasyauheni@gmail.com>
2020-07-12 21:49:58 +0300

    intermediate commit

--- src/js/sprint.js
+++ src/js/sprint.js
@@ -1,5 +1,6 @@
 import '../css/style.css';
 import '../css/sprint.css';
+import '../css/mode-page.css';
 import './sprint/components/start_page'
 import './sprint/components/timer'
 import './sprint/components/game_page'
@@ -10,14 +11,16 @@ import './sprint/helpers/show_word'
 import { createTimer } from './sprint/components/timer'
 
 import { gamePageInit } from './sprint/components/game_page'
+import { modePageInit } from './sprint/components/choose-mode_page'
+
+
 import { statisticsPageInit } from './sprint/components/statistics_page'
 
 document.querySelector('.start-btn').addEventListener('click', ()=>{
   document.querySelector('.start-page').classList.add('hidden');
-  gamePageInit()
+  modePageInit()
   createTimer(document.querySelector('.game-page__header'));
   statisticsPageInit()
-  console.log("HELLO")
 })
 
 document.addEventListener('click', (e)=> {
@@ -35,69 +38,6 @@ document.addEventListener('click', (e)=> {
 })
 
 
-localStorage.setItem('userId', '5f08bf2f76893e0017ec9809')
-localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMDhiZjJmNzY4OTNlMDAxN2VjOTgwOSIsImlhdCI6MTU5NDQwOTE4NSwiZXhwIjoxNTk0NDIzNTg1fQ.KZsGPBnaMx_IZ60DgORRkcuxw4OYrIsIdowxsO3VTfo')
-const urlHeroku = 'https://afternoon-falls-25894.herokuapp.com'
-  // {
-//   "message": "Authenticated",
-//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMDhiZjJmNzY4OTNlMDAxN2VjOTgwOSIsImlhdCI6MTU5NDQwOTE4NSwiZXhwIjoxNTk0NDIzNTg1fQ.KZsGPBnaMx_IZ60DgORRkcuxw4OYrIsIdowxsO3VTfo",
-//   "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMDhiZjJmNzY4OTNlMDAxN2VjOTgwOSIsInRva2VuSWQiOiI5NTQxZTZlNy1lOGVlLTQ0MDItYTZmZC0wOGM3ZmU4ZDAwMWQiLCJpYXQiOjE1OTQ0MDkxODUsImV4cCI6MTU5NDQyNTM4NX0.rphosPGr5aTaV7icgmOlplXfG7lfd1exWQqfy1FSw_I",
-//   "userId": "5f08bf2f76893e0017ec9809"
-// }
-
-
-async function response(rawResponse) {
-  if (!rawResponse.ok)  {
-    // if (rawResponse.status === 401) {
-    //   console.log()
-    // }
-    const data = await rawResponse.text();
-    console.log(data);
-    return null;
-  } else {
-    const content = await rawResponse.json();
-    return content;
-  }
-}
-
-async function getAgregateWords({ userId, group, wordsPerPage, filter }){
-  const filterUrl = `${encodeURIComponent(filter)}`;
-  const token = localStorage.getItem('token');
-  const rawResponse = await fetch(`${urlHeroku}/users/${userId}/aggregatedWords?wordsPerPage=${wordsPerPage}&filter=${filterUrl}`,
-      {
-          method: "GET",
-          withCredentials: true,
-          headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: "application/json",
-          },
-      }
-  );
-
-  return await response(rawResponse);
-};
-
-
-async function getLearnWords(value) {
-  const userId = localStorage.getItem('userId');
-  const agregateWords = {
-    "userId": `${userId}`,
-    "group": "",
-    "wordsPerPage": value,
-    "filter": `{"userWord.difficulty":"hard"}`,
-  }
-
-  const newWords = await getAgregateWords(agregateWords);
-  console.log('getLearnWords' + newWords);
-  return newWords;
-}
-
-getLearnWords(5)
-  .then(res => console.log(res))
-// async function aaa() {
-//   let a = await getLearnWords(10)
-// }
-// aaa()
 
 
 
