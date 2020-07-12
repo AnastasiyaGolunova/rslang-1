46665c94a6077700ad287c49bb829be2daf175f3
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-07-12 21:24:36 +0300

    feat: add commit for merge

---
+++ src/js/english-puzzle/images.js
@@ -0,0 +1,13 @@
+export default [
+    './img/english-puzzle/1.jpg',
+    './img/english-puzzle/2.jpg',
+    './img/english-puzzle/3.jpg',
+    './img/english-puzzle/4.jpg',
+    './img/english-puzzle/5.jpg',
+    './img/english-puzzle/6.jpg',
+    './img/english-puzzle/7.jpg',
+    './img/english-puzzle/8.jpg',
+    './img/english-puzzle/9.jpg',
+    './img/english-puzzle/10.jpg',
+    './img/english-puzzle/11.jpg',
+]
---
+++ src/js/savannah.js
@@ -0,0 +1,3 @@
+import "../css/style.css";
+import "../css/savannah.css";
+import "./gameSavannah.js";
