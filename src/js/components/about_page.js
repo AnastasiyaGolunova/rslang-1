ff4100427bfe8c9d94e46f7adbcc5a5f6f59149b
yauheniche <cherkasyauheni@gmail.com>
2020-07-12 22:47:53 +0300

    Revert "commit for switch branches"
    This reverts commit 6335fce0746cdc437459a0bb74c8a30af138b3c4.

---
+++ src/js/components/about_page.js
@@ -0,0 +1,76 @@
+const startPage = document.createElement('div');
+startPage.classList.add('wrapper')
+
+startPage.innerHTML =  `<div class="about-page">
+    <h2 class="game-name">Наша команда</h2>    
+    <div class="slider">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <div class="slide-header">
+          <div class="image"></div>
+          <div class="info">
+            <p class="status">team lead</p>
+            <p class="name">Тулепов Омирбек</p>
+            <a class="link" href="https://github.com/omirbeck" target="_blank"><i class="fab fa-github"></i>omirbeck</a>
+          </div>
+        </div>
+        <div class="deserts">
+          <p>Тим лид команды. В качестве тим лида координировал деятельность команды, занимался распределением задач, планированием,  помогал и консультировал в сложных ситуациях при написаний кода
+          Проводил еженедельные митинги с командой для уточнения требований, поставленных задач перед командой.</p>
+          <p>Реализована авторизация и регистрация нового пользователя.</p>
+          <p>Реализовал базовые функции приложения</p>
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+        <div class="slide-header">
+          <div class="image"></div>
+          <div class="info">
+            <p class="name">Голунова Анастасия</p>
+            <a class="link" href="https://github.com/anastasiyagolunova" target="_blank"><i class="fab fa-github"></i>Anastasiya Golunova</a>
+          </div>
+        </div>
+        <div class="deserts">
+          <p>Pазработка игры <span>speakIt</span>.</p>
+        </div>
+      </div>
+      
+      <div class="swiper-slide">
+        <div class="slide-header">
+          <div class="image"></div>
+          <div class="info">
+            <p class="name">Черкас Евгений</p>
+            <a class="link" href="https://github.com/yauheniche" target="_blank"><i class="fab fa-github"></i>yauheniche</a>
+          </div>
+        </div>
+        <div class="deserts">
+          <p>Pазработка игры <span>Sprint</span>.</p>
+          <p>Страница "О команде.</p>
+        </div>
+      </div>
+      
+      <div class="swiper-slide">
+        <div class="slide-header">
+          <div class="image"></div>
+          <div class="info">
+            <p class="name">Шапкарин Сергей</p>
+            <a class="link" href="https://github.com/SergeyShapkarin" target="_blank"><i class="fab fa-github"></i>Sergey Shapkarin</a>
+          </div>
+        </div>
+        <div class="deserts">
+          <p>Pазработка игры <span>English puzzle</span>.</p>
+        </div>
+      </div>
+
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+  </div>`
+
+document.body.append(startPage);
+
+// const rulesButton = document.querySelector('.rules-btn')
+
+// rulesButton.addEventListener('click', ()=>{
+//   document.querySelector('.game-rules').classList.toggle('hidden')
+// })
