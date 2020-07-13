051694ea3f950ee66b9be218ad534677030190b1
omirbeck <ommirtull@gmail.com>
2020-07-13 14:25:39 +0600

    refactor: fixed conflict in index

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import '../css/style.css';
 import '../css/dictionary.css';
 import '../css/fonts.css';
@@ -7,4 +5,3 @@ import '../css/game.css';
 import '../css/login.css';
 
 import './login.js';
->>>>>>> 39ee8f72e2d410603e243ff585c079f7cd0a2c03
